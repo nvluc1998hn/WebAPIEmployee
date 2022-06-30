9d2f648d993d1072c0b9d2d3d469fbbe8c47d30e
Luc NV <nvluc1998hn@gmail.com>
2022-06-30 16:34:32 +0700

    dgdfg

--- BackEnd/WebAPI/EfCore/ViewModels/UserInfo.cs
+++ BackEnd/WebAPI/EfCore/ViewModels/UserInfo.cs
@@ -8,17 +8,8 @@ namespace EfCore.ViewModels
 {
     public class UserInfo
     {
-        public int UserId { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public string AccountName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Adress { get; set; }
-        public bool IsDeleted { get; set; }
-        public bool IsActive { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public int CreatedBy { get; set; }
-        public int RoleId { get; set; }
+        public Guid EmployeeId { get; set; }
+        public string Token { get; set; }
+     
     }
 }
